f80adafce041fc12f3356d465cde8a440d1c0baf
piyodev <dev@piyolab.com>
2018-05-19 20:03:46 +0900

    add ropsten deploy setting

--- SmartContract/truffle.js
+++ SmartContract/truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MNEMONIC;
+var infuraAccessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +10,17 @@ module.exports = {
   		host: 'localhost',
   		port: 7545,
   		network_id: 5777
+  	},
+  	ropsten: {
+  		provider: function() {
+  			return new HDWalletProvider(
+  				mnemonic,
+  				'https://ropsten.infura.io/' + infuraAccessToken
+  			);
+  		},
+  		network_id: 3,
+  		gas: 2000000,
+  		gasPrice: 20000000000	// 20 Gwei
   	}
   }
 };
